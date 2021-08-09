83d5183442aea9014853da36f3d2685ec68ce5dd
YeongJin Kim <66052467+dudwls901@users.noreply.github.com>
2021-08-09 22:52:23 +0900

    2021-08-09

---
+++ for/boj_11021_A+B - 7.kt
@@ -0,0 +1,21 @@
+//https://www.acmicpc.net/problem/11021
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val t = Integer.parseInt(br.readLine())
+    for(i in 1 .. t){
+        val token = StringTokenizer(br.readLine())
+        bw.write("Case #$i: ${Integer.parseInt(token.nextToken())+Integer.parseInt(token.nextToken())}\n")
+    }
+    bw.flush()
+    bw.close()
+    br.close()
+}
