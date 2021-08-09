2a60bbbdeb27f972c3cb32b1cec015e2d1b1070d
YeongJin Kim <66052467+dudwls901@users.noreply.github.com>
2021-08-09 16:10:57 +0900

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,10 @@
+package com.example.algorithm;
+
+import java.util.Scanner;
+
+public class Java {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine());
+    }
+}
