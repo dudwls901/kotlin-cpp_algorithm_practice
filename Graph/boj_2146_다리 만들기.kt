3f1d544277d1fd825ee960ef6c71d1da4839e7c6
YeongJin Kim <66052467+dudwls901@users.noreply.github.com>
2022-06-02 01:58:01 +0900

    2021-12-19, 2022-06-02

--- Graph/boj_2146_다리 만들기.kt
+++ Graph/boj_2146_다리 만들기.kt
@@ -1,4 +1,5 @@
 //https://www.acmicpc.net/problem/2146
+//1
 import kotlin.math.*
 import java.util.*
 
@@ -73,3 +74,101 @@ fun main() = with(System.out.bufferedWriter()){
 
     close()
 }
+//2
+// import java.util.*
+// val br = System.`in`.bufferedReader()
+
+// fun getIntList() = br.readLine().split(' ').map { it.toInt() }
+// fun getInt() = br.readLine().toInt()
+
+
+// data class Node(
+//     val r: Int,
+//     val c: Int,
+//     val dis: Int
+// )
+// lateinit var graph: Array<IntArray>
+// lateinit var visited: Array<BooleanArray>
+// var answer = Int.MAX_VALUE
+// val dir = arrayOf(
+//     arrayOf(0,1),
+//     arrayOf(1,0),
+//     arrayOf(0,-1),
+//     arrayOf(-1,0)
+// )
+
+// fun bfs(r: Int, c: Int, n: Int, num: Int, type: String){
+//     if(type=="preSet") {
+//         val q: Queue<Pair<Int, Int>> = LinkedList()
+
+//         q.add(Pair(r, c))
+//         graph[r][c] = num
+
+//         while (q.isNotEmpty()) {
+//             val (cr, cc) = q.poll()
+//             for (i in 0 until 4) {
+//                 val nr = cr + dir[i][0]
+//                 val nc = cc + dir[i][1]
+//                 if (nr !in 0 until n || nc !in 0 until n) continue
+//                 if (graph[nr][nc] == 1) {
+//                     q.add(Pair(nr, nc))
+//                     graph[nr][nc] = num
+//                 }
+//             }
+//         }
+//     }
+//     else{
+//         val q: Queue<Node> = LinkedList()
+//         q.add(Node(r,c,0))
+//         visited[r][c] = true
+
+//         while(q.isNotEmpty()){
+//             val (cr, cc, cDis) = q.poll()
+
+//             for(i in 0 until 4){
+//                 val nr = cr + dir[i][0]
+//                 val nc = cc + dir[i][1]
+//                 if(nr !in 0 until n || nc !in 0 until n) continue
+//                 if(visited[nr][nc]) continue
+//                 if(graph[nr][nc] == num) continue
+//                 if(graph[nr][nc] == 0){
+//                     q.add(Node(nr,nc,cDis+1))
+//                     visited[nr][nc] = true
+//                 }
+//                 else{
+//                     answer = answer.coerceAtMost(cDis)
+//                     return
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+// fun main() = with(System.out.bufferedWriter()){
+
+//     //input
+//     val n = getInt()
+//     graph = Array(n){ getIntList().toIntArray() }
+
+//     //solve
+//     var num =2
+//     for(i in 0 until n){
+//         for(j in 0 until n){
+//             if(graph[i][j] == 1) {
+//                 bfs(i,j,n,num++, "preSet")
+//             }
+//         }
+//     }
+//     for(i in 0 until n){
+//         for(j in 0 until n){
+//             if(graph[i][j]!=0) {
+//                 visited = Array(n) { BooleanArray(n)}
+//                 bfs(i,j,n,graph[i][j],"solve")
+//             }
+//         }
+//     }
+//     //output
+//     write("$answer")
+//     close()
+// }
