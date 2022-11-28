9f847e071a2cf4c1543d2ef76c728864b26bd9e8
YeongJin Kim <66052467+dudwls901@users.noreply.github.com>
2022-11-28 20:33:54 +0900

    2022-11-28(다시 풀기)

--- Graph/boj_2021_최소 환승 경로.kt
+++ Graph/boj_2021_최소 환승 경로.kt
@@ -1,51 +1,48 @@
 //https://www.acmicpc.net/problem/2021
 import java.util.*
+val br = System.`in`.bufferedReader()
+fun getIntList() = br.readLine().trim().split(' ').map { it.toInt() }
+fun getInt() = br.readLine().trim().toInt()
 
-fun bfs(graph : Array<MutableList<Int>>, visited : IntArray, start : Int, end : Int ,n :Int) : Int {
-    val q : Queue<Int> = LinkedList<Int>()
-    q.add(start)
-    while(q.isNotEmpty()){
+lateinit var graph: Array<ArrayList<Int>>
+lateinit var visited: IntArray
+fun bfs(s: Int, e: Int, n: Int): Int {
+
+    val q: Queue<Int> = LinkedList()
+    q.add(s)
+    while (q.isNotEmpty()) {
         val cur = q.poll()
-        for(i in graph[cur].indices){
-            val next = graph[cur][i]
-            if(visited[next]>=0) continue
-            //호선
-            if(next>n){
+        for (next in graph[cur]) {
+            if (visited[next] >= 0) continue
+            if (next > n) {
                 q.add(next)
-                visited[next] = visited[cur]+1
+                visited[next] = visited[cur] + 1
             }
-            //도착
-            else if(next==end){
-                return visited[cur]
-            }
-            //역
+            else if(next == e) return visited[cur]
             else{
                 q.add(next)
-                visited[next]=visited[cur]
+                visited[next]  = visited[cur]
             }
         }
     }
     return -1
 }
 
-fun main()= with(System.out.bufferedWriter()){
-    val br = System.`in`.bufferedReader()
-    var tk = StringTokenizer(br.readLine())
-    val (n,m) = List(2){Integer.parseInt(tk.nextToken())}
-    val graph = Array(n*2+1,{mutableListOf<Int>()})
-    val visited = IntArray(n*2+1,{-1})
-    for(i in 1 .. m){
-        tk = StringTokenizer(br.readLine())
-        var num = Integer.parseInt(tk.nextToken())
-        while(num!=-1){
-            graph[num].add(n+i)
-            graph[n+i].add(num)
-            num = Integer.parseInt(tk.nextToken())
+fun main() = with(System.out.bufferedWriter()) {
+    //input
+    val (n, l) = getIntList()
+    graph = Array(n * 2+1) { ArrayList() }
+    visited = IntArray(n * 2+1) { -1 }
+    for (i in 1 .. l) {
+        getIntList().forEach {
+            if(it!=-1) {
+                graph[it].add(n + i)
+                graph[n + i].add(it)
+            }
         }
     }
-    tk = StringTokenizer(br.readLine())
-    val (start,end) = List(2){Integer.parseInt(tk.nextToken())}
-    write("${bfs(graph,visited,start,end,n)}")
+    val (s, e) = getIntList()
+    //solve, output
+    write("${bfs(s, e, n)}")
     close()
-
 }
